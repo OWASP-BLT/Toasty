313ef8617213533f00ebb66f77ebd16b6f09358b
Srijito354 <srijitoghosh113@outlook.com>
2025-12-02 02:00:51 +0530

    Updated prototype code to classes for easier package calling

---
+++ PR_analyzer/PR_prioritizer.py
@@ -0,0 +1,6 @@
+import os
+from github import Github
+from sarvamai import SarvamAI
+import PR_seeker
+
+PR_seeker
--- PR_analyzer/PR_seeker.py
+++ PR_analyzer/PR_seeker.py
@@ -1,15 +1,46 @@
 from github import Github
 import os
 
+
+'''
 f = open("Output.txt", "w")
 
 PERSONAL_TOKEN = os.getenv('PERSONAL_TOKEN')
 
 g = Github(PERSONAL_TOKEN)
-repo = g.get_repo("OWASP-BLT/Toasty")
+repo = g.get_repo("OWASP-BLT/BLT")
 
 pulls = repo.get_pulls(state='all')
 for pr in pulls:
     print(pr.title, pr.body)
     f.write(f"Title: {pr.title}\n")
     f.write(f"Body: {pr.body}\n\n")
+'''
+
+class Seeker:
+    def __init__(self):
+        self.PERSONAL_TOKEN = os.getenv('PERSONAL_TOKEN')
+        self.store_dict = {}
+        self.store_list = []
+        self.do_not_include = """<!-- START COPILOT CODING AGENT TIPS -->
+---
+
+💬 We'd love your input! Share your thoughts on Copilot coding agent in our [2 minute survey](https://gh.io/copilot-coding-agent-survey)."""
+
+        self.f = open("Output.txt", "w")
+    def forward(self, repository:str):
+        g = Github(self.PERSONAL_TOKEN)
+        repo = g.get_repo(repository)
+        pulls = repo.get_pulls(state='all')
+        store_list = []
+        for pr in pulls:
+            if pr != self.do_not_include:
+                store_dict = {}
+                store_dict['Title'] = pr.title
+                store_dict['Body'] = pr.body
+                store_list.append(store_dict)
+                self.f.writelines(store_list)
+        print(store_list)
+
+#seeker_obj = Seeker()
+#seeker_obj.forward("OWASP-BLT/Toasty")
